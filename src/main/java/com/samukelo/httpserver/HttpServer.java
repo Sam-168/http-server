817b397ec6020c03a42f0e334302abea17aedad6
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-10 22:37:11 +0200

    initialised git repo and setup

---
+++ src/main/java/com/samukelo/httpserver/HttpServer.java
@@ -0,0 +1,7 @@
+package com.samukelo.httpserver;
+//Driver class for http server
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Server starting...");
+    }
+}
