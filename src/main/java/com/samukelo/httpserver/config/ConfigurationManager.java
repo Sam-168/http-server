58e72ae6b5326900c60027726165077d2fb4f9c0
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-11 23:06:00 +0200

    added configuration files and json file

---
+++ src/main/java/com/samukelo/httpserver/config/ConfigurationManager.java
@@ -0,0 +1,5 @@
+package com.samukelo.httpserver.config;
+
+public class ConfigurationManager {
+    private static ConfigurationManager myConfigurationManager; //Just need one configuration manager that will be shared across the prj
+}
