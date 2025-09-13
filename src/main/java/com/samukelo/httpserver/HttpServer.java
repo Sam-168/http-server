33ff24a14c2a0269526ff62c5f71a0448985beb7
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-13 02:19:42 +0200

    Created my socket to accept connections listening to a port

--- src/main/java/com/samukelo/httpserver/HttpServer.java
+++ src/main/java/com/samukelo/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.samukelo.httpserver;
 import com.samukelo.httpserver.config.Configuration;
 import com.samukelo.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 //Driver class for http server
 public class HttpServer {
     public static void main(String[] args) {
@@ -12,5 +18,32 @@ public class HttpServer {
 
         System.out.println("Port: "+ conf.getPort());
         System.out.println("Webroot: " + conf.getWebroot());
+
+        //A server socket that is going to listen to a specific port
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+           Socket socket = serverSocket.accept(); //prompts a socket to accept a connection(listening to a port)
+
+            InputStream inputStream = socket.getInputStream();//Reading something from the socket(get Input Stream)
+            OutputStream outputStream = socket.getOutputStream(); //write to socket
+
+            //Reading
+
+            //Writing
+            String html = "<html><head><title>Java HTTP server</title></head><body><h1>Can you believe it?My own http server!</h1></body></html>";
+
+            String response =
+                    "HTTP/1.1 200 OK";//Status line(response) : HTTP VERSION RESPONSE CODE RESPONSE MESSAGE
+
+
+
+            //Closing resources
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
