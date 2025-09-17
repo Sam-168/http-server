35f436351b41ee206510e2596422ac2061b77f9e
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-18 01:50:28 +0200

    defining get, set methods for our httpMethod instance variable

---
+++ src/main/java/com/samukelo/http/HttpMethod.java
@@ -0,0 +1,6 @@
+package com.samukelo.http;
+
+public enum HttpMethod {
+    //Defining GET and HEAD
+
+}
--- src/main/java/com/samukelo/http/HttpRequest.java
+++ src/main/java/com/samukelo/http/HttpRequest.java
@@ -4,4 +4,17 @@ public class HttpRequest extends HttpMessage{
     private String method;
     private String requestTarget;
     private String httpVersion;
+
+    public String getMethod() {
+        return method;
+    }
+
+    void setMethod(String method) {
+        this.method = method;
+    }
+
+    HttpRequest(){
+
+    }
+
 }
