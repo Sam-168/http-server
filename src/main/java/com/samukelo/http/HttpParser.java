fde1df016b496c7e70bc361f6d88effed956f078
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-18 02:27:49 +0200

    created a loop where we look for the CR LF chars in method parseRequestLine

--- src/main/java/com/samukelo/http/HttpParser.java
+++ src/main/java/com/samukelo/http/HttpParser.java
@@ -3,6 +3,7 @@ package com.samukelo.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -13,7 +14,12 @@ public class HttpParser {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
 
-    public HttpRequest parseHttpRequest(InputStream inputStream){
+    //Need to define ASCII -> HEXADECIMAL FOR SP, CR and LF
+    private static final int SP = 0x20;
+    private static final int CR = 0x0D;
+    private static final int LF = 0x0A;
+
+    public HttpRequest parseHttpRequest(InputStream inputStream) throws IOException {
 
         InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
 
@@ -25,7 +31,17 @@ public class HttpParser {
         return request;
 
     }
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request) {
+    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
+        //Loop where we look for the carriage return line feed chars
+        int _byte;
+        while ((_byte = reader.read()) >= 0){
+            if (_byte == CR){
+                _byte = reader.read();
+                if (_byte == LF){
+                    return;
+                }
+            }
+        }
     }
 
     private void parseBody(InputStreamReader reader, HttpRequest request) {
