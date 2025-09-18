5229cf74213cb0058c9e12f82d6d272e86365151
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-18 02:16:35 +0200

    added getter and constructor to construct and return error code

--- src/main/java/com/samukelo/http/HttpParsingException.java
+++ src/main/java/com/samukelo/http/HttpParsingException.java
@@ -2,5 +2,14 @@ package com.samukelo.http;
 
 public class HttpParsingException extends Exception{
 
-    private String errorCode;
+    private final HttpStatusCode errorCode;
+
+    public HttpParsingException(HttpStatusCode errorCode) {
+        super(errorCode.MESSAGE);
+        this.errorCode = errorCode;
+    }
+
+    public HttpStatusCode getErrorCode() {
+        return errorCode;
+    }
 }
