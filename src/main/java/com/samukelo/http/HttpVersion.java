c3f552271e1c24f7f53b6cb8de057a6dfb7e700e
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-24 20:43:25 +0200

    added code to create http version with regex pattern and error validation

--- src/main/java/com/samukelo/http/HttpVersion.java
+++ src/main/java/com/samukelo/http/HttpVersion.java
@@ -1,5 +1,8 @@
 package com.samukelo.http;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum HttpVersion {
     HTTP_1_1("HTTP/1.1", 1, 1);
 //code to create http version
@@ -11,5 +14,28 @@ public enum HttpVersion {
         this.LITERAL = LITERAL;
         this.MAJOR = MAJOR;
         this.MINOR = MINOR;
+    }
+    private static final Pattern httpVersionRegexPattern = Pattern.compile("^HTTP/(?<major>\\d+).(?<minor>\\d+)");
+    public static HttpVersion getBestCompatibleVersion(String literalVersion) throws HttpParsingException {
+        Matcher matcher = httpVersionRegexPattern.matcher(literalVersion);
+        if (!matcher.find() || matcher.groupCount() != 2){
+            throw new HttpParsingException(HttpStatusCode.SERVER_ERROR_500_INTERNAL_SERVER_ERROR);
+        }
+        int major = Integer.parseInt(matcher.group("major"));
+        int minor = Integer.parseInt(matcher.group("minor"));
+
+        HttpVersion tempBestCompatible = null;
+        for (HttpVersion version : HttpVersion.values()){
+            if (version.LITERAL.equals(literalVersion)){
+                return version;
+            }else{
+                if (version.MAJOR == major){
+                    if (version.MINOR < minor){
+                        tempBestCompatible = version;
+                    }
+                }
+            }
+        }
+        return tempBestCompatible;
     }
 }
