804dfbdb9cc21c9c74317ffcd953f5b86fc1f2a9
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-24 22:40:13 +0200

    created and tested method for a bad http version test

--- src/test/java/com/samukelo/http/HttpVersionTest.java
+++ src/test/java/com/samukelo/http/HttpVersionTest.java
@@ -19,7 +19,16 @@ public class HttpVersionTest {
 
         assertNotNull(version);
         assertEquals(version, HttpVersion.HTTP_1_1);
+    }
+    @Test
+    void getBestCompatibleVersionBadFormat(){
+        HttpVersion version = null;
+        try {
+            version = HttpVersion.getBestCompatibleVersion("http/1.1");
+            fail();
+        } catch (BadHttpVersionException e) {
 
+        }
 
     }
 }
