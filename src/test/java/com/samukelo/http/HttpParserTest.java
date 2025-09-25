937462df0d601ab2a9c4ecd646e97030571caf5c
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-25 04:06:38 +0200

    added method and test case for a bad http version

--- src/test/java/com/samukelo/http/HttpParserTest.java
+++ src/test/java/com/samukelo/http/HttpParserTest.java
@@ -96,6 +96,18 @@ class HttpParserTest {
         }
 
     }
+    @Test
+    void parseHttpRequestBadHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadHttpVersionTestCase()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
 
 
     //method to generate test cases
@@ -184,6 +196,31 @@ class HttpParserTest {
         String rawData = "GET / HTTP/1.1\r" + //no line feed(LF)
                 "Host: localhost:8080\r\n" +
 
+                "Accept-Language: en-US,en;q=0.9\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(
+                StandardCharsets.US_ASCII
+        )
+        );
+        return inputStream;
+    }
+    //JUnit for bad http format
+    private InputStream generateBadHttpVersionTestCase(){
+        String rawData = "GET / HTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Microsoft Edge\";v=\"140\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
                 "Accept-Language: en-US,en;q=0.9\r\n" +
                 "\r\n";
 
