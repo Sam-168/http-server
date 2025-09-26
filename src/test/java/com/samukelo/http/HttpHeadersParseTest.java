4cea567db076c635d28565f635390926a168c70d
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-26 15:04:15 +0200

    added code to test a single header with its test case

--- src/test/java/com/samukelo/http/HttpHeadersParseTest.java
+++ src/test/java/com/samukelo/http/HttpHeadersParseTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HttpHeadersParseTest {
@@ -25,7 +27,37 @@ public class HttpHeadersParseTest {
     }
 
     @Test
-    public void TestPrivateMethod() throws InvocationTargetException, IllegalAccessException {
-        parseHeadersMethod.invoke(httpParser, new InputStreamReader(new ByteArrayInputStream("".getBytes())),new HttpRequest());
+    public void testSimpleSingleHeader() throws InvocationTargetException, IllegalAccessException {
+        HttpRequest request = new HttpRequest();
+        parseHeadersMethod.invoke(httpParser,
+                generateSimpleSingleHeaderMessage(),
+                request);
+    }
+
+    private InputStreamReader generateSimpleSingleHeaderMessage() {
+        String rawData = "Host: localhost:8080\r\n";
+
+            //    "Connection: keep-alive\r\n" +
+          //      "sec-ch-ua: \"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Microsoft Edge\";v=\"140\"\r\n" +
+         //       "sec-ch-ua-mobile: ?0\r\n" +
+         //       "sec-ch-ua-platform: \"Windows\"\r\n" +
+         //       "Upgrade-Insecure-Requests: 1\r\n" +
+         //       "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0\r\n" +
+        //        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+        //        "Sec-Fetch-Site: none\r\n" +
+        //        "Sec-Fetch-Mode: navigate\r\n" +
+        //        "Sec-Fetch-User: ?1\r\n" +
+        //        "Sec-Fetch-Dest: document\r\n" +
+       //         "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+       //         "Accept-Language: en-US,en;q=0.9\r\n" +
+      //          "\r\n"
+
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(
+                StandardCharsets.US_ASCII
+            )
+        );
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
+        return reader;
     }
 }
