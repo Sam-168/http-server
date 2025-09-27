48d5f012ee52fd319e3b556ec02c107c1ebb75a8
iamsam786 <samukelondlela2.0@gmail.com>
2025-09-27 05:33:35 +0200

    added code for testing error before space header

--- src/test/java/com/samukelo/http/HttpHeadersParseTest.java
+++ src/test/java/com/samukelo/http/HttpHeadersParseTest.java
@@ -46,6 +46,21 @@ public class HttpHeadersParseTest {
         assertEquals(14, request.getHeaderNames().size()); //Why 14?
         assertEquals("localhost:8080", request.getHeader("host"));
     }
+    @Test
+    public void testErrorBeforeSpaceColonHeader() throws IllegalAccessException {
+        HttpRequest request = new HttpRequest();
+        try {
+            parseHeadersMethod.invoke(httpParser,
+                    generateSpaceBeforeColonError(),
+                    request);
+        } catch (InvocationTargetException e) {
+            if (e.getCause() instanceof HttpParsingException){
+                assertEquals(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST, (HttpParsingException)e.getCause());
+            }
+
+        }
+
+    }
 
     private InputStreamReader generateSimpleSingleHeaderMessage() {
         String rawData = "Host: localhost:8080\r\n";
@@ -100,6 +115,31 @@ public class HttpHeadersParseTest {
         return reader;
     }
 
-    
+    private InputStreamReader generateSpaceBeforeColonError() {
+        String rawData = "Host : localhost:8080\r\n\r\n";
+
+        //    "Connection: keep-alive\r\n" +
+        //      "sec-ch-ua: \"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Microsoft Edge\";v=\"140\"\r\n" +
+        //       "sec-ch-ua-mobile: ?0\r\n" +
+        //       "sec-ch-ua-platform: \"Windows\"\r\n" +
+        //       "Upgrade-Insecure-Requests: 1\r\n" +
+        //       "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0\r\n" +
+        //        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+        //        "Sec-Fetch-Site: none\r\n" +
+        //        "Sec-Fetch-Mode: navigate\r\n" +
+        //        "Sec-Fetch-User: ?1\r\n" +
+        //        "Sec-Fetch-Dest: document\r\n" +
+        //         "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+        //         "Accept-Language: en-US,en;q=0.9\r\n" +
+        //          "\r\n"
+
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(
+                StandardCharsets.US_ASCII
+        )
+        );
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
+        return reader;
+    }
 }
 
